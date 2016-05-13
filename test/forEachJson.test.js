1c2064110114f2d718423eeadf04618c3b2eaaa5
Stephen Zuniga <stephen.zuniga001@gmail.com>
2016-05-13 18:21:01 -0400

    Add JSON parsers

---
+++ test/forEachJson.test.js
@@ -0,0 +1,153 @@
+/* jshint node:true, mocha: true */
+
+var stream = require('stream');
+var _ = require('lodash');
+var expect = require('chai').expect;
+var forEachJson = require('../lib/forEachJson.js');
+
+describe('[forEachJson]', function() {
+    var data = ['{"json":"test"}', new Buffer('{"item":"json"}'), '{"item1":"item2"}'];
+
+    function runTest(stream, done) {
+        var idx = 0;
+
+        function onData(chunk) {
+
+            expect(chunk).to.be.an('object');
+            expect(chunk).to.deep.equal(JSON.parse(data[idx]));
+
+            idx++;
+        }
+
+        function onEnd(err) {
+            expect(arguments).to.have.length(0);
+            expect(idx).to.equal(data.length);
+
+            done();
+        }
+
+        forEachJson(stream, onData, onEnd);
+    }
+
+    it('iterates through a Readable stream', function(done) {
+        var readableStream = new stream.Readable();
+
+        readableStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(readableStream, done);
+    });
+
+    it('iterates through a Readable object stream', function(done) {
+        var readableStream = new stream.Readable({
+            objectMode: true
+        });
+
+        readableStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(readableStream, done);
+    });
+
+    it('returns an error for a Readable stream', function(done) {
+        var readableStream = new stream.Readable();
+
+        readableStream._read = (function() {
+            var d = data.slice().concat(['{"non":"json}']);
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        forEachJson(readableStream, _.noop, function(err) {
+            expect(arguments).to.have.length(1);
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.message).to.equal('Unexpected end of JSON input');
+            done();
+        });
+    });
+
+    it('iterates through a Duplex stream', function(done) {
+        var duplexStream = new stream.Duplex();
+
+        duplexStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(duplexStream, done);
+    });
+
+    it('iterates through a Duplex object stream', function(done) {
+        var duplexStream = new stream.Duplex({
+            objectMode: true
+        });
+
+        duplexStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(duplexStream, done);
+    });
+
+    it('returns an error for a Duplex stream', function(done) {
+        var duplexStream = new stream.Duplex();
+
+        duplexStream._read = (function() {
+            var d = data.slice().concat(['{"non":"json}']);
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        forEachJson(duplexStream, _.noop, function(err) {
+            expect(arguments).to.have.length(1);
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.message).to.equal('Unexpected end of JSON input');
+            done();
+        });
+    });
+});
---
+++ test/waitJson.test.js
@@ -0,0 +1,149 @@
+/* jshint node:true, mocha: true */
+
+var stream = require('stream');
+var expect = require('chai').expect;
+var waitJson = require('../lib/waitJson.js');
+
+describe('[waitJson]', function() {
+    var data = ['[', '{"json":"test"}', new Buffer(',{"item":"json"}'), ',{"item1":"item2"}', ']'];
+
+    function runTest(stream, done) {
+
+        function onEnd(err, content) {
+            expect(arguments).to.have.length(2);
+
+            expect(err).to.equal(null);
+
+            expect(content).to.be.an('array');
+            expect(content).to.deep.equal(JSON.parse(Buffer.concat(data.map(function(item) {
+                return new Buffer(item);
+            }))));
+
+            done();
+        }
+
+        waitJson(stream, onEnd);
+    }
+
+    it('waits for a Readable stream', function(done) {
+        var readableStream = new stream.Readable();
+
+        readableStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(readableStream, done);
+    });
+
+    it('waits for a Readable object stream', function(done) {
+        var readableStream = new stream.Readable({
+            objectMode: true
+        });
+
+        readableStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(readableStream, done);
+    });
+
+    it('returns an error for a Readable stream', function(done) {
+        var readableStream = new stream.Readable();
+
+        readableStream._read = (function() {
+            var d = data.slice(0, -1).concat([',{"non":"json}', ']']);
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        waitJson(readableStream, function(err) {
+            expect(arguments).to.have.length(1);
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.message).to.equal('Unexpected end of JSON input');
+            done();
+        });
+    });
+
+    it('waits for a Duplex stream', function(done) {
+        var duplexStream = new stream.Duplex();
+
+        duplexStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(duplexStream, done);
+    });
+
+    it('waits for a Duplex object stream', function(done) {
+        var duplexStream = new stream.Duplex({
+            objectMode: true
+        });
+
+        duplexStream._read = (function() {
+            var d = data.slice();
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        runTest(duplexStream, done);
+    });
+
+    it('returns an error for a Duplex stream', function(done) {
+        var duplexStream = new stream.Duplex();
+
+        duplexStream._read = (function() {
+            var d = data.slice(0, -1).concat([',{"non":"json}', ']']);
+
+            return function() {
+                if (d.length > 0) {
+                    this.push(d.shift());
+                } else {
+                    this.push(null);
+                }
+            };
+        })();
+
+        waitJson(duplexStream, function(err) {
+            expect(arguments).to.have.length(1);
+            expect(err).to.be.an.instanceof(Error);
+            expect(err.message).to.equal('Unexpected end of JSON input');
+            done();
+        });
+    });
+});
