c7c9f6e73bcf3f719cbd4faaa36fa0e21f2284b0
Stephen Zuniga <stephen.zuniga001@gmail.com>
2016-08-13 11:38:05 -0400

    Improve build

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var sequence = require('gulp-sequence');
+var beeper = require('beeper');
 
 var source = {
   js: ['*.js', 'lib/**/*.js', 'performance/**/*.js', 'test/**/*.js'],
   lib: ['lib/**/*.js'],
-  test: ['test/**/*.js']
+  perf: ['performance/**/*.test.js'],
+  test: ['test/**/*.test.js']
 };
 
 gulp.task('lint', function() {
   return gulp.src(source.js)
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.results(function(results) {
+      if (results.warningCount || results.errorCount) {
+        beeper();
+      }
+    }))
+    .pipe(eslint.format())
+    .pipe(gulpif(gulp.seq.indexOf('watch') < 0, eslint.failAfterError()));
 });
 
 gulp.task('test', function() {
@@ -40,7 +49,9 @@ gulp.task('coverage', ['pre-coverage'], function() {
 });
 
 gulp.task('watch', ['default'], function() {
-  gulp.watch(source.js, sequence('lint', 'test'));
+  gulp.watch(source.js, function() {
+    sequence(['lint', 'test'])();
+  });
 });
 
 gulp.task('default', sequence('lint', 'test'));
