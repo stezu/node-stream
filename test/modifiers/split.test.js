cf600188e2a42c33265e556f800135c9b9cc6cd7
Stephen Zuniga <stephen.zuniga001@gmail.com>
2016-10-02 15:01:47 -0400

    add tests for #split

--- test/modifiers/split.test.js
+++ test/modifiers/split.test.js
@@ -1,54 +1,86 @@
-// var expect = require('chai').expect;
+var expect = require('chai').expect;
 
-// var getReadableStream = require('../_utilities/getReadableStream.js');
-// var getDuplexStream = require('../_utilities/getDuplexStream.js');
-// var split = require('../../').split;
+var getReadableStream = require('../_utilities/getReadableStream.js');
+var getDuplexStream = require('../_utilities/getDuplexStream.js');
+var runBasicStreamTests = require('../_utilities/runBasicStreamTests.js');
+var split = require('../../').split;
 
-// describe('[split]', function () {
-//   var data = ['it\nem1', new Buffer('item2'), 'item3\n', 'i\ntem4'];
-//   var expected = ['it', 'em1item2item3', 'i', 'tem4'];
+describe('[split]', function () {
+  var data = ['it\nem1', new Buffer('item2'), 'item3\n', 'i\ntem4'];
+  var expected = ['it', 'em1item2item3', 'i', 'tem4'];
 
-//   function runTest(stream, done) {
-//     var idx = 0;
-//     var actual = [];
+  function runTest(stream, objectMode, done) {
+    var idx = 0;
+    var actual = [];
 
-//     stream
-//       .pipe(split())
-//       .on('data', function (chunk) {
-//         actual.push(chunk.toString());
-//       })
-//       .on('error', done)
-//       .on('end', function () {
-//         expect(actual).to.deep.equal(expected);
-//         done();
-//       });
-//   }
+    stream
+      .pipe(split())
+      .on('data', function (chunk) {
+        actual.push(chunk.toString());
+      })
+      .on('error', done)
+      .on('end', function () {
+        expect(actual).to.deep.equal(expected);
+        done();
+      });
+  }
 
-//   it('splits a Readable stream', function (done) {
-//     var readableStream = getReadableStream(data);
+  runBasicStreamTests(data, data, runTest);
 
-//     runTest(readableStream, done);
-//   });
+  it('accepts a regex matcher as the first argument', function (done) {
+    var testData = ['mary had ', 'a lit', 'tle lamb who', 'se fleece was white ', 'as sn', 'ow'];
+    var expected = ['mary', 'had', 'a', 'little', 'lamb', 'whose', 'fleece', 'was', 'white', 'as', 'snow'];
+    var readableStream = getReadableStream(testData);
+    var actual = [];
 
-//   it('splits a Readable object stream', function (done) {
-//     var readableStream = getReadableStream(data, {
-//       objectMode: true
-//     });
+    readableStream
+      .pipe(split(/\s+/))
+      .on('data', function (chunk) {
+        actual.push(chunk);
+      })
+      .on('error', done)
+      .on('end', function () {
+        expect(actual).to.deep.equal(expected);
 
-//     runTest(readableStream, done);
-//   });
+        done();
+      });
+  });
 
-//   it('splits a Duplex stream', function (done) {
-//     var duplexStream = getDuplexStream(data);
+  it('accepts a mapper as the first argument', function (done) {
+    var testData = ['{"json": "parser"}\n{"parsing": "', 'json"}\n{"is', '":"fun"}\n'];
+    var expected = [{ json: 'parser' }, { parsing: 'json' }, { is: 'fun'}];
+    var readableStream = getReadableStream(testData);
+    var actual = [];
 
-//     runTest(duplexStream, done);
-//   });
+    readableStream
+      .pipe(split(JSON.parse))
+      .on('data', function (chunk) {
+        actual.push(chunk);
+      })
+      .on('error', done)
+      .on('end', function () {
+        expect(actual).to.deep.equal(expected);
 
-//   it('splits a Duplex object stream', function (done) {
-//     var duplexStream = getDuplexStream(data, {
-//       objectMode: true
-//     });
+        done();
+      });
+  });
 
-//     runTest(duplexStream, done);
-//   });
-// });
+  it('accepts both a string matcher and a mapper as arguments', function (done) {
+    var testData = ['{"json": "parser"}banana{"parsing": "', 'json"}banana{"is', '":"fun"}'];
+    var expected = [{ json: 'parser' }, { parsing: 'json' }, { is: 'fun'}];
+    var readableStream = getReadableStream(testData);
+    var actual = [];
+
+    readableStream
+      .pipe(split('banana', JSON.parse))
+      .on('data', function (chunk) {
+        actual.push(chunk);
+      })
+      .on('error', done)
+      .on('end', function () {
+        expect(actual).to.deep.equal(expected);
+
+        done();
+      });
+  });
+});
