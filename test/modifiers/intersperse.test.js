4ff9b030bcb4f324e5fb1ff9777838b475d9081c
Stephen Zuniga <stephen.zuniga001@gmail.com>
2016-11-19 18:42:48 -0500

    fix lint errors

--- test/modifiers/intersperse.test.js
+++ test/modifiers/intersperse.test.js
@@ -1,7 +1,5 @@
 var expect = require('chai').expect;
 
-var getReadableStream = require('../_utilities/getReadableStream.js');
-var getDuplexStream = require('../_utilities/getDuplexStream.js');
 var runBasicStreamTests = require('../_utilities/runBasicStreamTests.js');
 var intersperse = require('../../').intersperse;
 
