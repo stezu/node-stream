c40212f43e056a3eccf8131416c2a61ccd3463fc
Kiril Vatev <vatev.1@gmail.com>
2017-01-08 13:11:49 -0500

    implementing the "time" options in throttle/batch

--- test/modifiers/throttle.test.js
+++ test/modifiers/throttle.test.js
@@ -11,83 +11,103 @@ function buff(str) {
   return new Buffer(str);
 }
 
-describe('[throttle]', function () {
-  var data = ['item1', new Buffer('item2'), 'item3', '', 'item4'];
-  var objData = [true, false, [1, 2, 3], 'string', 0, '11', 95.23, { obj: true }, _.noop];
+describe.only('[throttle]', function () {
 
-  function runTest(stream, objectMode, done) {
-    var expected = [[buff('item1')], [buff('item2'), buff('item3'), buff('item4')]];
-    var objExpected = [
-      objData.slice(0, 1),
-      objData.slice(1)
-    ];
-    var actual = [];
+  describe('when "time" is defined in options', function () {
+    var data = ['item1', new Buffer('item2'), 'item3', '', 'item4'];
+    var objData = [true, false, [1, 2, 3], 'string', 0, '11', 95.23, { obj: true }, _.noop];
 
-    stream
-      .pipe(throttle(5))
-      .on('data', function (chunk) {
-        actual.push(chunk);
-      })
-      .on('error', done)
-      .on('end', function () {
+    function runTest(stream, objectMode, done) {
+      var expected = [[buff('item1')], [buff('item2'), buff('item3'), buff('item4')]];
+      var objExpected = [
+        objData.slice(0, 1),
+        objData.slice(1)
+      ];
+      var actual = [];
 
-        if (objectMode) {
-          expect(actual).to.deep.equal(objExpected);
-        } else {
-          expect(actual).to.deep.equal(expected);
-        }
+      stream
+        .pipe(throttle({ time: 5 }))
+        .on('data', function (chunk) {
+          actual.push(chunk);
+        })
+        .on('error', done)
+        .on('end', function () {
 
-        done();
+          if (objectMode) {
+            expect(actual).to.deep.equal(objExpected);
+          } else {
+            expect(actual).to.deep.equal(expected);
+          }
+
+          done();
+        });
+    }
+
+    runBasicStreamTests(data, objData, runTest);
+
+    it('writes all chunks as an array', function (done) {
+      var input = getReadableStream([1], {
+        objectMode: true
       });
-  }
 
-  runBasicStreamTests(data, objData, runTest);
-
-  it('writes all chunks as an array', function (done) {
-    var input = getReadableStream([1], {
-      objectMode: true
+      input
+        .pipe(throttle({ time: 1 }))
+        .on('data', function (chunk) {
+          expect(chunk).to.be.an('array');
+        })
+        .on('error', done)
+        .on('end', done);
     });
 
-    input
-      .pipe(throttle(1))
-      .on('data', function (chunk) {
-        expect(chunk).to.be.an('array');
-      })
-      .on('error', done)
-      .on('end', done);
+    it('combines data written during an interval into one write', function (done) {
+      var actual = [];
+      var input = through.obj();
+
+      input
+        .pipe(throttle({ time: 1 }))
+        .on('data', function (chunk) {
+          actual.push(chunk);
+        })
+        .on('error', done)
+        .on('end', function () {
+          expect(actual).to.deep.equal([[1], [2, 3], [4, 5]]);
+
+          done();
+        });
+
+      input.write(1);
+
+      setTimeout(function () {
+        input.write(2);
+        input.write(3);
+      }, 1);
+
+      setTimeout(function () {
+        input.write(4);
+        input.write(5);
+
+        input.end();
+      }, 4);
+    });
+
+    it('successfully ends if there is no data written');
+
   });
 
-  it('combines data written during an interval into one write', function (done) {
-    var actual = [];
-    var input = through.obj();
+  describe('when "count" is defined in options', function () {
+    it('writes all chunks as an array');
 
-    input
-      .pipe(throttle(1))
-      .on('data', function (chunk) {
-        actual.push(chunk);
-      })
-      .on('error', done)
-      .on('end', function () {
-        expect(actual).to.deep.equal([[1], [2, 3], [4, 5]]);
+    it('emits chunks of the defined size');
 
-        done();
-      });
-
-    input.write(1);
-
-    setTimeout(function () {
-      input.write(2);
-      input.write(3);
-    }, 1);
-
-    setTimeout(function () {
-      input.write(4);
-      input.write(5);
-
-      input.end();
-    }, 4);
+    it('successfully ends if there is no data written');
   });
 
-  it('successfully ends if there is no data written');
+  describe('when both "time" and "count" are defined in options', function () {
+    it('writes chunks if count is met first before the defined time');
+
+    it('writes chunks if time is met first before the defined count');
+
+    it('successfully ends if there is no data written');
+  });
 
 });
