76a153f8a5ad7aa2840dd3eba1fdd332d8a481ce
Kiril Vatev <vatev.1@gmail.com>
2017-01-22 16:54:40 -0500

    making #flatten basic tests expectations more clear

--- test/modifiers/flatten.test.js
+++ test/modifiers/flatten.test.js
@@ -16,9 +16,7 @@ describe('[flatten]', function () {
     var data = ['item1', 'item2', 'item3'];
     var expected = data.map(buff);
     var objData = [{ a: 1}, [1, 2, 3], true, 'string', 42, _.noop];
-    var objExpected = objData.reduce(function (memo, val) {
-      return memo.concat(val);
-    }, []);
+    var objExpected = [{ a: 1}, 1, 2, 3, true, 'string', 42, _.noop];
 
     function runTest(stream, objectMode, done) {
       var actual = [];
