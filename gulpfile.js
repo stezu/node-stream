f2b075b8c0ba4be0a3f0f9b301086008094ab734
Stephen Zuniga <stephen.zuniga001@gmail.com>
2018-03-05 20:51:38 -0800

    remove everything but docs from gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,4 @@
 var gulp = require('gulp');
-var gulpif = require('gulp-if');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
-var sequence = require('gulp-sequence');
-var beeper = require('beeper');
 var packageJson = require('./package.json');
 
 var majorVersion = process.version.slice(1).split('.')[0];
@@ -16,53 +11,6 @@ var source = {
 var dest = {
   docs: './docs'
 };
-
-gulp.task('lint', function () {
-  var eslint;
-
-  if (majorVersion < 4) {
-    // eslint-disable-next-line no-console
-    return console.log('ESLint cannot run on Node', process.version);
-  }
-
-  // require eslint once we've determined we can
-  // eslint-disable-next-line global-require
-  eslint = require('gulp-eslint');
-
-  return gulp.src(source.js)
-    .pipe(eslint())
-    .pipe(eslint.results(function (results) {
-      if (results.warningCount || results.errorCount) {
-        beeper();
-      }
-    }))
-    .pipe(eslint.format())
-    .pipe(gulpif(gulp.seq.indexOf('watch') < 0, eslint.failAfterError()));
-});
-
-gulp.task('test', function () {
-  return gulp.src(source.test)
-    .pipe(mocha());
-});
-
-gulp.task('coverage-instrument', function () {
-  return gulp.src(source.lib)
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('coverage-report', function () {
-  return gulp.src(source.test)
-    .pipe(istanbul.writeReports())
-    .pipe(istanbul.enforceThresholds({
-      thresholds: {
-        global: 98,
-        each: 90
-      }
-    }));
-});
-
-gulp.task('coverage', sequence('coverage-instrument', 'test', 'coverage-report'));
 
 gulp.task('docs', function () {
   var documentation;
@@ -86,15 +34,3 @@ gulp.task('docs', function () {
     }))
     .pipe(gulp.dest(dest.docs));
 });
-
-gulp.task('watch:run', function (done) {
-  sequence('lint', 'test')(done);
-});
-
-gulp.task('watch', ['watch:run'], function () {
-  gulp.watch(source.js, ['watch:run']);
-});
-
-gulp.task('build', sequence('lint', 'coverage', 'docs'));
-
-gulp.task('default', sequence('lint', 'test', 'docs'));
